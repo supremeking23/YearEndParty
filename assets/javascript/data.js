f28a85a4c246f892d8f108e8b1efdb76cd6881ec
Keikishim2 <Keikishim2@gmail.com>
2021-12-21 13:16:31 +0800

    spelling

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,6 +1,6 @@
 export default [
     {
-        name: "Kie Kishimoto",
+        name: "Kei Kishimoto",
         position: "Front End Developer",
         video_greeting_link: "https://ik.imagekit.io/1cqtok8fv7n/VID_20211221103528_EMEwlrpMWCC.mp4?updatedAt=1640055099633",
         video_greeting_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
@@ -25,7 +25,7 @@ export default [
     },
     {
         name: "Fitz Gerald Villegas",
-        position: "Backend End Developer",
+        position: "Back End Developer",
         video_greeting_link: "https://ik.imagekit.io/1cqtok8fv7n/3d_ocean_1590675653_R6yyTGVOW.mov?updatedAt=1639441884594",
         video_greeting_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         gcash_number: "09260917276",
